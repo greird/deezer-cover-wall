62fec0a50ab146709c42a06e37f405550e09b6c0
greird <h.fauconnier@gmail.com>
2016-04-23 19:46:27 +0200

    Moving player.js content to init.js

--- js/player.js
+++
@@ -1,9 +0,0 @@
-DZ.init({
-  appId  : '16dd244abec147dac3d6155c6a5383e4',
-  channelUrl : 'http://localhost:8888/deezer-cover-wall/channel.html',
-  player: {
-		onload: function(response) {
-			console.log('DZ.player is ready', response);
-		}
-	}
-});
